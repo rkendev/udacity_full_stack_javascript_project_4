f7304dbf038d4c1932a164377eb060e8029dbc66
rkendev <aspjscript@hotmail.com>
2021-11-05 23:32:00 +0100

    bean stalk config changes

--- udagram-api/src/config/config.ts
+++ udagram-api/src/config/config.ts
@@ -8,7 +8,7 @@ export const config = {
   port: Number(process.env.DATABASE_PORT),
   host: process.env.POSTGRES_HOST,
   dialect: "postgres",
-  aws_region: process.env.AWS_REGION,
+  aws_region: process.env.AWS_DEFAULT_REGION,
   //aws_region: "eu-central-1",
   aws_profile: process.env.AWS_PROFILE,
   aws_media_bucket: process.env.AWS_BUCKET,
