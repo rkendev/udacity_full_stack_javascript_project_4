e5d7666d8a40d63584faa2785bb44d0c012cf911
rkendev <aspjscript@hotmail.com>
2021-11-06 12:15:48 +0100

    commit to test circle ci

--- udagram-api/src/server.ts
+++ udagram-api/src/server.ts
@@ -60,7 +60,7 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
     res.send("/api/v0/");
   });
 
-  // Start the Server
+  // Start the Server//
   app.listen(process.env.PORT, () => {
     console.log(`server running ${process.env.URL}`);
     console.log(`press CTRL+C to stop server`);
